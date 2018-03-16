c1cd10a0ce40a47aec706e47628fc96d3378723e
ponmakhaj <ponmakhaj@104-06-2017.bi.local>
2018-03-16 10:41:27 -0400

    asdf

--- HashFunction/src/TicTacToeHashCode.java
+++ HashFunction/src/TicTacToeHashCode.java
@@ -2,37 +2,52 @@
 
 public class TicTacToeHashCode extends Board {
 
- boolean [] winners;  // True if the hash string that maps to this index is a winner, false otherwise
-    
-  TicTacToeHashCode(String s) {
-   super(s);
-  // TODO Instantiate/fill winners array.  
-  }
-  
-  // TODO - write the myHashCode function.  It must create a unique hashcode for all of the 
-  //   possible values the game board (3 ^ 9) and it MUST use the super.charAt(row, col) function
-  @Override
-    public int myHashCode() {
+	boolean[] winners; // True if the hash string that maps to this index is a winner, false otherwise
 
-      return 0;
-   }
-   
-    public boolean isWin(String s) {
-    // return the value in the winner array for the hash chode of the board string sent in.
-    return true;
-    }
-  
-   public static void main(String[] args) throws InterruptedException {
-      TicTacToeHashCode board = new TicTacToeHashCode ("Tic Tac Toe");
-      while (true) {
-      
-         String currentBoard = board.boardValues[(int)(Math.random()* board.boardValues.length)];
-         board.show(currentBoard);
-         board.setHashCode(board.myHashCode());
-         // TODO Update this line to call your isWin method.
-         board.setWinner(TicTacToe.isWin(currentBoard));
-         
-         Thread.sleep(4000);      
-      }
-   }
+	TicTacToeHashCode(String s) {
+		super(s);
+		// TODO Instantiate/fill winners array.
+	}
+
+	// TODO - write the myHashCode function. It must create a unique hashcode for
+	// all of the
+	// possible values the game board (3 ^ 9) and it MUST use the super.charAt(row,
+	// col) function
+	@Override
+	public int myHashCode() {
+		int pow3[][] = { { 1, 3, 9 }, { 27, 81, 243 }, { 729, 2187, 6561 } };
+
+		int num = 0;
+
+		for (int i; i < TicTacToe.ROWS; i++) {
+			for (int j = 0; i < TicTacToe.COLS; i++) {
+				char ch = super.charAt(i, j);
+
+				num += pow3[i][j];
+			}
+
+		}
+
+		return num;
+	}
+
+	public boolean isWin(String s) {
+		// return the value in the winner array for the hash chode of the board string
+		// sent in.
+		return true;
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		TicTacToeHashCode board = new TicTacToeHashCode("Tic Tac Toe");
+		while (true) {
+
+			String currentBoard = board.boardValues[(int) (Math.random() * board.boardValues.length)];
+			board.show(currentBoard);
+			board.setHashCode(board.myHashCode());
+			// TODO Update this line to call your isWin method.
+			board.setWinner(TicTacToe.isWin(currentBoard));
+
+			Thread.sleep(4000);
+		}
+	}
 }
